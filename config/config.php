572aeef4fbdae7bd44e1decca558c333cd54b59d
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-11-06 10:07:37 -0500

    initial commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$Host = "";
+$Name = "";
+$User = "";
+$Pass = "";
+$Style = "/recept/config/style.css";
+
+$PHP_SELF = $_SERVER['PHP_SELF'];
+define(WEBSTAT, false);
+define(GOOGLE, false);
+?>
